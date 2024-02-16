25c941546f9985f34233fc720d289e9b37a59aaf
Buenaventura.298870 <Buenaventura.298870@LAB-A-PC31>
2024-02-16 09:20:56 +0800

    Add project files.

---
+++ DataTypesApp/DataTypesProgram.cs
@@ -0,0 +1,19 @@
+﻿namespace DataTypesApp
+{
+    internal class DataTypesProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the pieces of apples: ");
+            int pieces = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter total price of " +  pieces + " apple(s): ");
+            double TotalPrice = Convert.ToDouble(Console.ReadLine());   
+
+            Console.WriteLine("The total price of 6 apple(s) is " + TotalPrice);
+            Console.WriteLine("The value of original price is " + TotalPrice);
+            int ConvertedValue = (int)TotalPrice;
+            Console.WriteLine("The value of converted price is " + ConvertedValue);
+        }
+    }
+}
